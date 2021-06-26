0b7d28805e132ee0fa976e76eaa49362e548e80d
Umut Ulaş AYDIN <umut200134@gmail.com>
2021-06-26 23:07:30 +0300

    register and login page added

---
+++ login.py
@@ -0,0 +1,73 @@
+import tkinter as tk
+
+
+def register():
+
+    register = tk.Tk()
+    register.geometry("250x250")
+    register.title("Password Manager")
+
+    
+    register.columnconfigure(0, weight = 1, minsize = 75)
+    register.columnconfigure(1, weight = 1, minsize = 75)
+    register.rowconfigure(0, weight = 1, minsize = 1)
+    register.rowconfigure(1, weight = 1, minsize = 1)
+    register.rowconfigure(2, weight = 1, minsize = 1)
+
+
+    l1 = tk.Label(register, text = "Nickname")
+    l1.grid(row = 0, column = 0)
+
+    l2 = tk.Label(register, text = "Password")
+    l2.grid(row = 1, column = 0)
+
+    v1 = tk.StringVar()
+    v2 = tk.StringVar()
+
+    e1 = tk.Entry(register, textvariable = v1)
+    e1.grid(row = 0, column = 1)
+
+    e2 = tk.Entry(register, textvariable = v2)
+    e2.grid(row = 1, column = 1)
+
+    b1 = tk.Button(register, text = "Sign Up")
+    b1.grid(row = 2, column = 1, padx = 3, pady = 3)
+
+    register.mainloop()
+
+
+def login():
+
+    login = tk.Tk()
+    login.geometry("250x250")
+    login.title("Password Manager")
+
+    
+    login.columnconfigure(0, weight = 1, minsize = 75)
+    login.columnconfigure(1, weight = 1, minsize = 75)
+    login.rowconfigure(0, weight = 1, minsize = 1)
+    login.rowconfigure(1, weight = 1, minsize = 1)
+    login.rowconfigure(2, weight = 1, minsize = 1)
+
+
+    l1 = tk.Label(login, text = "Nickname")
+    l1.grid(row = 0, column = 0)
+
+    l2 = tk.Label(login, text = "Password")
+    l2.grid(row = 1, column = 0)
+
+    v1 = tk.StringVar()
+    v2 = tk.StringVar()
+
+    e1 = tk.Entry(login, textvariable = v1)
+    e1.grid(row = 0, column = 1)
+
+    e2 = tk.Entry(login, textvariable = v2)
+    e2.grid(row = 1, column = 1)
+
+    b1 = tk.Button(login, text = "Sign In")
+    b1.grid(row = 2, column = 1, padx = 3, pady = 3)
+
+    login.mainloop()
+
+login()
